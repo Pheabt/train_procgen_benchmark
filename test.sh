588895866f771cf6907c1ee4cb0939efda2f508f
Phoenix <31894252+pheabt@users.noreply.github.com>
2023-08-16 18:23:04 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=your_job_name
+#SBATCH --job-name=procgen01
 #SBATCH --output=output.log
 #SBATCH --error=error.log
-#SBATCH --partition=gpu  # 选择适合GPU的分区
-#SBATCH --gres=gpu:1     # 请求1个GPU
-#SBATCH --mem=8G        # 指定内存
-#SBATCH --time=5:00:00  # 预估运行时间
+#SBATCH --partition=gpu01 
+#SBATCH --gres=gpu:1    
 
-# 激活您的conda环境
-source /work/home/acdjbrxlyg/anaconda3/bin/activate pro  # 替换为您的实际环境路径和环境名
+source /home/iotsc_g4/app/miniconda3/bin/activate pro 
+cd /home/iotsc_g4/aaa/train_procgen_benchmark/main.py
 
-# 切换到存放main.py的目录
-cd /work/home/acdjbrxlyg/train_procgen_benchmark/main.py
-
-# 运行Python脚本
 python main.py --gpu 0
